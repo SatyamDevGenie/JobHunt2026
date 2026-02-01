67cc531015ecd10b958737fcdf58ed9d00738c6c
SatyamDevGenie <satyamsawant54@gmail.com>
2026-02-01 23:07:23 +0530

    Basic Backend Server Setup

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
+const app = express();
+
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+    origin: "http://localhost:5173",
+    credentials: true
+}
+
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+    res.send("JobHunt API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
