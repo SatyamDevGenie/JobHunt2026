2df5a0a6d67718f73c22a6102057fc8f12ce58d6
SatyamDevGenie <satyamsawant54@gmail.com>
2026-02-15 18:01:05 +0530

    made changes -- job.model.js

--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -40,4 +40,4 @@ const jobSchema = new mongoose.Schema({
     }]
 
 }, { timestamps: true });
-export const job = mongoose.model("Job", jobSchema);
+export const Job = mongoose.model("Job", jobSchema);
