13b42e67df01e326e9ed24e46de2f3a55e978a23
SatyamDevGenie <satyamsawant54@gmail.com>
2026-02-15 16:39:55 +0530

    added logout -- user.routes.js

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import {register, login, updateProfile} from "../controllers/user.controller.js"
+import {register, login, updateProfile, logout} from "../controllers/user.controller.js"
 
 import isAuthentication from "../middlewares/isAuthenticated.js";
 
@@ -8,6 +8,7 @@ const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/logout").get(logout)
 router.route("/profile/update").post(isAuthentication, updateProfile);
 
 
